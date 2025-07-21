ea1686e40a56264e81b123921ebc8f560404030e
CoolGuy <96452706+PedrinFranca@users.noreply.github.com>
2025-07-21 18:48:31 -0300

    Project Init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pedrinfranca/yooza',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '218324f1594b8c0bcbd6032254616c57aad1a6be',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pedrinfranca/yooza' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '218324f1594b8c0bcbd6032254616c57aad1a6be',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
